477559372329ed3d61ae5b8e6f8c96cfa56e0b93
Sehtaj Singh <sehtaj@192.168.1.9>
2025-02-11 23:27:39 +0530

    submitted isAnagram.py

---
+++ Neetcode/isAnagram.py
@@ -0,0 +1,42 @@
+#Purpose: IsAnagram(s,t) checks if two letters are anagrams or no
+#isAnagram: Str, Str -> Bool
+
+#Time complexity: O(n)
+#Space complexity: O(n)
+
+def isAnagram(s,t):
+    s = s.lower()
+    t = t.lower()
+
+    if len(s) != len(t):
+        return False
+    
+    seen = {}
+
+    for char in s:
+        if char in seen:
+            seen[char] += 1
+        else:
+            seen[char] = 1
+
+    for char in t:
+        if char in seen:
+            seen[char] -= 1
+        else:
+            seen[char] = 1
+
+    for key in seen:
+        if seen[key] != 0:
+            return False
+    
+    return True
+
+#edge cases
+isAnagram("", " ") #False
+isAnagram("carrace", "RaceCar") #True
+isAnagram("ab", "ac")#False
+isAnagram("a", "a") #True
+
+
+
+
